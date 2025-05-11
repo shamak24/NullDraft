2a02802a1180621f551d2b26a8d6594510ac2fbc
Shamak <rishitroyr2535@gmail.com>
2025-05-11 17:11:47 +0530

    Refactor blog post structure and enhance UI components

--- index.js
+++ index.js
@@ -9,17 +9,35 @@ app.get('/', (req,res)=>{
     res.render('index.ejs');
 })
 
-app.get('/newPost', (req,res) => {
+app.get('/newpost', (req,res) => {
     res.render('newpost.ejs');
 })
 
 app.get('/posts', (req,res) => {
-    res.render('posts.ejs');
+    res.render('posts.ejs', {
+        posts: blogs
+    });
 })
 
 app.use((req, res) => {
     res.status(404).render('404.ejs');
 });
+
+// app.post('/newpost/submit', (req,res)=>{
+//     const date = new Date().toLocaleString('en-US');
+//     const { title, description, image, author, content } = req.body;
+//     const newPost = {
+//         id: blogs.length + 1,
+//         title,
+//         date,
+//         description,
+//         image,
+//         author,
+//         content
+//     };
+//     blogs.push(newPost);
+//     res.redirect('/posts');
+// })
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
