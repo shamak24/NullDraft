50be0f5f33eb6655b2bc7f737cfe4832f5e6d37d
Shamak <rishitroyr2535@gmail.com>
2025-05-12 00:31:55 +0530

    Implement CRUD functionality for blog posts and enhance post display

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import express from 'express';
-import blogs from './Posts.js';
 import bodyParser from 'body-parser';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const postsPath = path.join(__dirname, 'Posts.json');
+
 //MIDDLEWARE FOR EJS
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+function readPosts() {
+    if (!fs.existsSync(postsPath)) return [];
+    const data = fs.readFileSync(postsPath, 'utf-8');
+    return JSON.parse(data);
+}
+
+function writePosts(posts){
+    fs.writeFileSync(postsPath, JSON.stringify(posts, null, 2), 'utf-8');
+}
 
 
 app.get('/', (req,res)=>{
@@ -21,12 +35,14 @@ app.get('/newpost', (req,res) => {
 })
 
 app.get('/posts', (req,res) => {
+    const blogs = readPosts();
     res.render('posts.ejs', {
         posts: blogs
     });
 })
 
 app.get('/posts/:id', (req,res)=>{
+    const blogs = readPosts();
     const postId = req.params.id;
     const post = blogs.find((blog) => blog.id == postId);
     if (post) {
@@ -39,9 +55,16 @@ app.get('/posts/:id', (req,res)=>{
 })
 
 app.post('/submit', (req,res)=>{
+    const blogs = readPosts();
     const date = new Date().toLocaleString();
+    const id = blogs.length+1;
     const { title, description, author, content } = req.body;
-    console.log('Form Data:', { author, title, date, description, content });
+    const newFormData = {id: id, title: title, author: author, description: description, content: content, date: date};
+    blogs.push(newFormData);
+    writePosts(blogs);
+    res.render("posts.ejs", {
+        posts: blogs
+    });
 })
 
 
